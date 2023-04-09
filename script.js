9009bff2be68084ba444da14b1209e0e21759943
GeekeyCoder <geekycoder886@gmail.com>
2023-04-10 00:07:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Web Developer','Cloud Enthusiast'],
+    strings: ['Web Developer','Tech Savvy','Cloud Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
